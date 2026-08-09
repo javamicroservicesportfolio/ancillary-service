bd4020b6123f37eb664d7f20fa63da318bd70a39
Keith <karirukeith@gmail.com>
2026-08-09 18:09:48 +0300

    initial commit

---
+++ src/main/java/com/portfolio/ancillaryservice/AncillaryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.ancillaryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AncillaryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AncillaryServiceApplication.class, args);
+    }
+
+}
